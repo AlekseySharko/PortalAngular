a8a52e41f7c350faeb33bde1c9f2c2484c9c33da
AlekseySharko <alekseysharkodeveloper@gmail.com>
2021-06-22 17:29:06 +0300

    Minor changes

--- src/app/main/catalog/catalog-products/moderating/products/product-template/image-gallery/image-gallery.component.ts
+++ src/app/main/catalog/catalog-products/moderating/products/product-template/image-gallery/image-gallery.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ProductImage} from "../../../../../catalog-classes/products/product-image";
 
 @Component({
   selector: 'app-image-gallery',
@@ -6,27 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./image-gallery.component.css']
 })
 export class ImageGalleryComponent implements OnInit {
-  images: string[] = [];
-  selectedImage: string = '';
-  constructor() {
-    this.images.push("https://content2.onliner.by/catalog/device/main/8d9186c7db3e7235287a7b665eb3640f.jpeg");
-    this.images.push("https://content2.onliner.by/catalog/device/main/08dddddab2be478b8cef2cf39599adbd.jpeg");
-    this.images.push("https://content2.onliner.by/catalog/device/main/1d8204d6d25eb86f33dd14c6db3f8457.jpeg");
-    this.images.push("https://content2.onliner.by/catalog/device/main/2c647744d184aac78c7f8c0aab26e548.jpeg");
-    this.images.push("https://content2.onliner.by/catalog/device/main/8d637629362ba948d658f8bfdfbf4da6.jpeg");
-    this.images.push("https://content2.onliner.by/catalog/device/main/6d7c98e6a5f91f48296e5164f6779b71.jpeg");
-    this.images.push("https://content2.onliner.by/catalog/device/main/c00db043775ddd1618bdd75c4a699099.jpeg");
+  @Input() images: ProductImage[] = [];
+  selectedImage: string = 'https://icon-library.com/images/image-placeholder-icon/image-placeholder-icon-16.jpg';
+  constructor() {}
 
-
+  ngOnInit(): void {
     this.setInitialImage();
   }
 
-  ngOnInit(): void {
+  onAddImage() {
 
   }
+
   setInitialImage() {
-    if(this.images.length > 0) {
-      this.selectedImage = this.images[0];
+    if (this.images.length > 0) {
+      this.selectedImage = this.images[0].address;
     }
   }
 }
